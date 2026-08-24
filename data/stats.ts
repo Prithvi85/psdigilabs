16b0d33b2ff613098ab8316819abc040b16eecf0
Prithwish Saha <prithwishsaha85@hotmail.com>
2026-08-24 20:42:49 +0530

    Update professional experience statistic

--- data/stats.ts
+++ data/stats.ts
@@ -1,6 +1,6 @@
 export const stats = [
   { value: "3+", label: "Projects Completed" },
-  { value: "3+", label: "Years Experience" },
+  { value: "10+", label: "Years Professional Experience" },
   { value: "2", label: "Live Websites" },
   { value: "1", label: "Android App in Development" },
   { value: "100%", label: "Commitment" },
